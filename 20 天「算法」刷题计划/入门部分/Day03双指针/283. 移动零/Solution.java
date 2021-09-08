84486f7c5082bd767a17eaca2a7df5efcbc4dedb
TAOTE <36142033+gyafen@users.noreply.github.com>
2021-09-08 22:37:23 +0800

    Update Solution.java

--- 20 天「算法」刷题计划/入门部分/Day03双指针/283. 移动零/Solution.java
+++ 20 天「算法」刷题计划/入门部分/Day03双指针/283. 移动零/Solution.java
@@ -4,22 +4,28 @@ class Solution {
         int nzerop = 0;
         int length = nums.length;
 
+        //第一个0的位置
         while (zerop<length && nums[zerop] != 0){
             zerop++;
         }
         nzerop = zerop+1;
+        //第一个0后面第一个非0位置
         while (nzerop < length && nums[nzerop] == 0){
             nzerop++;
         }
 
         while (nzerop < length){
+            //交换
             int tmp = nums[nzerop];
             nums[nzerop] = nums[zerop];
             nums[zerop] = tmp;
 
+
+            //下一个0
             while (zerop < length && nums[zerop] != 0){
                 zerop++;
             }
+            //下一个非0
             while (nzerop < length && nums[nzerop] == 0){
                 nzerop++;
             }
